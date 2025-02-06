6c2ed79df1092207a6d0b62ef5c602c44a363232
Alvaro Ramos <alvarosantiagosilva03@gmail.com>
2025-02-06 08:35:38 -0300

    Create A.cs

---
+++ A.cs
@@ -0,0 +1,15 @@
+class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			for (int i = 0; i <= 100; i += 2)
+			{
+			
+				Console.WriteLine(i);
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
